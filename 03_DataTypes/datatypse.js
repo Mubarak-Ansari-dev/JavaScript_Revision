5e1e9cfd5fa12b8f5a15ff121a2bd888ef22aeb9
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-05-30 16:53:14 +0530

    feat: 03_DataTypes In JavaScript

---
+++ 03_DataTypes/datatypse.js
@@ -0,0 +1,36 @@
+// Data Types in JavaScript
+// Data types
+
+// Number
+let num = 3.14;
+let num2 = 15;
+
+// Strings
+
+const firstName = "Mubarak";
+
+console.log(firstName);
+
+// Booleans
+ let isLearning = true;
+//  console.log(isLearning);
+
+// NULL
+ let islearning = null;
+//  console.log(islearning);
+// NULL-> Variable has been declared but has no value assigned to it. It represents the intentional absence of any object value.
+
+// UNDEFINED
+let city = undefined;
+// console.log(city);
+
+//objects
+ const person = {
+    firstName: "tuyio",
+    lastName: "gyuio",
+    age:90,
+    isprogrammer:true,
+ }
+ console.log(person);
+
+ // Objects me key-value pairs hote hain, jisme key ek string hoti hai aur value kisi bhi data type ki ho sakti hai
