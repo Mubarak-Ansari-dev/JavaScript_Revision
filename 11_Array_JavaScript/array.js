d8797537c47cfeeda36255eae840c101f392a8b3
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-06-04 20:29:14 +0530

    feat(js): Array in JavaScript

---
+++ 11_Array_JavaScript/array.js
@@ -0,0 +1,26 @@
+
+
+//Array in JavaScript
+// Array is a collection of similar type of data elements
+
+// when we want to use multiple values in a single variable
+
+const students = ["Rahman", "Shafiq", "Shohag", "Shafiq"];
+
+console.log(students.length); // size of array
+console.log(students[0]); // to access first value
+
+students[2] = "Mubarak"; // to change(mutate) value
+
+students.push("Ali"); // to add value in the end
+
+students.pop(); // to remove value from the end
+console.log(students);
+
+students.reverse()  // to reverse the array
+console.log(students.indexOf("Shafiq")); // to find index of a value
+
+
+const myArray = [1, true, "Ansari", "😎"]
+// means array is a collection of different type of data in javaScript unlike- Java, C++
+
